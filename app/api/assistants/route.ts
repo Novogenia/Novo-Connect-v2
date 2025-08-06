b8e994e780da00ef391e4cef9200187802013c0a
Novogenia <ceo@novogenia.com>
2025-08-06 17:30:10 +0200

    Update route.ts

--- app/api/assistants/route.ts
+++ app/api/assistants/route.ts
@@ -1,5 +1,5 @@
 import { assistantId } from "../../utils/assistant-config";
-import { openai } from "../utils/openai"; 
+import { openai } from "utils/openai"; 
 
 
 export const runtime = "nodejs";
